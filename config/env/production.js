0779d217c7fa3a7048e91395ef83de2a6b27d89c
Chintan <chintan@wohlig.com>
2017-10-18 15:01:27 +0530

    done

--- config/env/production.js
+++ config/env/production.js
@@ -26,7 +26,7 @@ module.exports = {
    ***************************************************************************/
 
   port: 80,
-  realHost: "http://tushar.io",
+  realHost: "http://cloud.unifli.aero",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com"]
 
   /***************************************************************************
