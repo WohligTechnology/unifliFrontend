71f873c739a85caac6614b8dd2df9afcc380a285
ShaikhNargis1406 <nargis.shaikh@wohlig.com>
2017-11-14 18:26:22 +0530

    responsive

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -149,7 +149,17 @@ firstapp.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $lo
     $urlRouterProvider.otherwise("/");
     $locationProvider.html5Mode(isproduction);
 });
-
+firstapp.config(function(toastrConfig) {
+    angular.extend(toastrConfig, {
+      autoDismiss: false,
+      containerId: 'toast-container',
+      maxOpened: 0,    
+      newestOnTop: true,
+      positionClass: 'toast-top-right',
+      preventDuplicates: false,
+      preventOpenDuplicates: true
+    });
+  });
 firstapp.directive('img', function ($compile, $parse) {
     return {
         restrict: 'E',
